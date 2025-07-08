3329e7427804e5e598c794280b7adaf209c292b5
AdityaShett <adityashetty559@gmail.com>
2025-07-07 23:55:02 -0400

    Initial website

--- portfolio-setup.sh
+++ portfolio-setup.sh
@@ -1,7 +1,7 @@
 # In terminal or command prompt:
 cd path/to/your/website
 git init
-git remote add origin https://github.com/yourusername/personal-website.git
+git remote add origin https://github.com/AdityaShett/personal-website.git
 git add .
 git commit -m "Initial website"
 git push -u origin master
